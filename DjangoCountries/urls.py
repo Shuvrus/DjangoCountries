24c025a2a2a83a966cd6959aff4194845ccaa458
Ruslan <shuvalov.r@mail.ru>
2023-05-11 00:27:18 +0300

    change 11.05.23-1

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('countries-list/', views.countries_list),
-    path('countries-list/<str:country>/', views.one_country),
-    path('languages/', views.languages_list)
+    path('', views.home, name='home'),
+    path('countries-list/', views.countries_list, name='countries-list'),
+    path('countries-list/<str:country>/', views.one_country, name='country'),
+    path('languages/', views.languages_list, name='languages-list')
 ]
